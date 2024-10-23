6a358bae6a0d1d78b137fb072fb36b2352514658
Miletza <anmilet@gmail.com>
2024-10-22 21:41:59 -0500

    solution create with api project, entities and repositories

---
+++ src/MusicStore.Entities/Genre.cs
@@ -0,0 +1,9 @@
+﻿namespace MusicStore.Entities;
+
+public class Genre
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = default!; //también podría ser: = string.Empty
+    public bool Status { get; set; } = true;
+}
+
